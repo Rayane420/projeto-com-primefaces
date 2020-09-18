801060f17d9f0639403a00ead9537606ebcf90e2
Rayane Ribeiro <46855211+Rayane420@users.noreply.github.com>
2020-09-17 21:59:48 -0300

    Adicionando web service de cep
    Adicionando o web service de cep e salvando os dados no cadastro do usuário

--- pos-java-maven-hibernate/src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ pos-java-maven-hibernate/src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -1,5 +1,10 @@
 package managedBean;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +13,9 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.google.gson.Gson;
 
 import dao.DaoUsuario;
 import model.UsuarioPessoa;
@@ -44,6 +52,43 @@ public class UsuarioPessoaManagedBean {
 		list.add(usuarioPessoa);
 		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Informação:","Salvo com sucesso!"));
 		return "usuario-salvo";
+	}
+	
+	
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+		try {
+			
+			URL url = new URL("https://viacep.com.br/ws/"+usuarioPessoa.getCep()+"/json/");
+			URLConnection  connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new  InputStreamReader(is, "UTF-8"));
+			
+			String cep = "";
+			StringBuilder jsonCep = new StringBuilder();
+			
+			while ((cep = br.readLine()) != null) {
+				jsonCep.append(cep);
+				
+			}
+			//objeto auxiliar pegando dados do cep
+			UsuarioPessoa userCepPessoa = new Gson().fromJson(jsonCep.toString(), UsuarioPessoa.class);
+			
+			//passando dados do objeto auxiliar para a pessoa
+			usuarioPessoa.setCep(userCepPessoa.getCep());
+			usuarioPessoa.setLogradouro(userCepPessoa.getLogradouro());
+			usuarioPessoa.setComplemento(userCepPessoa.getComplemento());
+			usuarioPessoa.setBairro(userCepPessoa.getBairro());
+			usuarioPessoa.setLocalidade(userCepPessoa.getLocalidade());
+			usuarioPessoa.setUf(userCepPessoa.getUf());
+			usuarioPessoa.setUnidade(userCepPessoa.getUnidade());
+			usuarioPessoa.setIbge(userCepPessoa.getIbge());
+			usuarioPessoa.setGia(userCepPessoa.getGia());
+
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 	public String novo() {
